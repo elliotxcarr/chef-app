2bb5cd03d7f4ffd3093637c18640993e37fdae7c
Elliot Carr <Elliot.Carr@navtor.com>
2025-07-09 14:01:30 +0100

    added tailwind, selection panel setup

--- src/app/selection-panel/selection-panel.component.ts
+++ src/app/selection-panel/selection-panel.component.ts
@@ -1,3 +1,5 @@
+
+
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
@@ -9,10 +11,28 @@ import { Component } from '@angular/core';
     styleUrl: './selection-panel.component.css'
 })
 export class SelectionPanelComponent {
-  items = {
-    carbs: ['Rice', 'Potatoes', 'Bread', 'Sweet Potatoes'],
-    fruit: ['Oranges', 'Apples', 'Banana', 'Strawberries'],
-    veg: ['Peas', 'Carrots', 'Broccoli', 'Cabbage'],
-    meat:['Beef', 'Chicken', 'Fish', 'Pork']
-  }
+  items = [
+    {
+      category: 'carbs',
+      label: 'Carbohydrates',
+      foods: ['Rice', 'Potatoes', 'Bread', 'Sweet Potatoes']
+    },
+    {
+      category: 'fruit',
+      label: 'Fruits',
+      foods: ['Oranges', 'Apples', 'Banana', 'Strawberries']
+    },
+    {
+      category: 'veg',
+      label: 'Vegetables',
+      foods: ['Peas', 'Carrots', 'Broccoli', 'Cabbage']
+    },
+    {
+      category: 'meat',
+      label: 'Meats & Protein',
+      foods: ['Beef', 'Chicken', 'Fish', 'Pork']
+    }
+  ];
+
+  categories = Object.keys(this.items)
 }
